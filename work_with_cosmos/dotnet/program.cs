2d4167e656421fa5ca88aca4fca10382826ac6fb
amycoughlin-acg <72401415+amycoughlin-acg@users.noreply.github.com>
2023-05-09 16:34:27 -0500

    Update program.cs

--- work_with_cosmos/dotnet/program.cs
+++ work_with_cosmos/dotnet/program.cs
@@ -19,24 +19,31 @@ class Program
             string my_connection_string = "[ConnectionString]";
 
             //Declare a CosmosClient, called myClient, using the connection string
+            
             CosmosClient myClient = new (my_connection_string);
 
+
             //Declare a Database, called myDatabase, from the Cosmos DB account called *exactly* "LabDBNet";
+            
             Database myDatabase = myClient.GetDatabase("LabDBNet");
 
             //Declare a ContainerProperties object, called containerProps, with the minimum properties:
-            //  "LabItemsNet" *exactly* for the container name and "/labPK" for the partition key path;
+            //  "LabItemsNet" *exactly* for the container name and "/labPK" for the partition key path.
             //  NOTE: There is a way to create a container directly without the use of the ContainerProperties
             //  class. However, using ContainerProperties as a habit makes it easier to define additional
             //  container properties when you create a new container.
             
-            //You get this line of code as a "freebie":
+            //***You get this line of code as a "freebie"***
             ContainerProperties containerProps = new ContainerProperties("LabItemsNet","/labPK");
 
-            //Declare Container, called myContainer, on myDatabase, passing containerProps as a single parameter.
-            Container myContainer = myDatabase.GetDatabase(containerProps);
+
+            //Declare a Container, called myContainer, on myDatabase, 
+            //  passing containerProps as a single parameter.
             
-            //Use the GenericItem class to create an item object, called myItem, to be upserted to the container.
+            Container myContainer = myDatabase.GetContainer(containerProps);
+            
+            //Use the GenericItem class (just outside of the Main method) to create an item object, 
+            //  called myItem, to be upserted to the container.
             //IMPORTANT: the id property should be assigned a GUID. Assign this value to that property:
             //    "70b63682-b93a-4c77-aad2-65501347265f". If you create an item with that same GUID, the
             //    upsert will overwrite the previous item. Alter the GUID value slightly if you decide
@@ -45,22 +52,25 @@ class Program
 
             GenericItem myItem = new(
               id: "70b63682-b93a-4c77-aad2-65501347265f",
-              labPK: 5,
-              name: "This is my string"
+              itemName: "This is my string",
+              labPK: 5
             );
-            
-            
-            
+                       
             //Upsert your item to the container you created
-            // Hint: The Main method is an async Task, so instantiate ItemReponse object to "kick off"
+            // Hint: The Main method is an async Task, so instantiate and ItemReponse object to "kick off"
             // the task. Your code will start like this:
-            //                                 ItemResponse response = await . . . 
+            //                            ItemResponse response = await . . . 
             
             ItemResponse item = await this.container.CreateItemAsync(test, new PartitionKey(test.status));
 
-
     }
-    
+        
+        public class GenericItem
+        {
+            public Guid id {get; set;}
+            public string? itemName {get; set;}
+            public int labPK {get; set;} 
+        }   
     
   }
 }       
